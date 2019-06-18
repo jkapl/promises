64f058176a4fc7a875a848ac39380559abb1c506
jkapl <joelkaplan1@gmail.com>
2019-06-18 12:27:13 -0700

    passed getStatusCode

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,14 +6,36 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, cb) {
   // TODO
+  fs.readFile(filePath, 'utf8',(err, data) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      let newLineIndex = data.indexOf('\n');
+      cb(null, data.slice(0,newLineIndex));
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, cb) {
   // TODO
+  request(url, (err, response, body) => {
+      if(err) {
+        cb(err, null)
+      } else {
+        cb(null, response.statusCode)
+      }
+  })
 };
+
+
+// request('http://www.google.com', function (error, response, body) {
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
